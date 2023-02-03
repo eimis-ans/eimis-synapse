dc309902699531d8d5565ec5d944b27386861f35
Pipo Bimbo <heroinedor@gmail.com>
2023-02-03 18:56:44 +0100

    test to suppress region and endpoint

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     bucket = "terraform-states-hp-sandbox"
     key    = "terraform.tfstate"
-    region = "gra"
+    #region = "gra"
     #or sbg or any activated high performance storage region
-    endpoint                    = "s3.gra.perf.cloud.ovh.net"
+    #endpoint                    = "s3.gra.perf.cloud.ovh.net"
     skip_credentials_validation = true
     skip_region_validation      = true
   }
