1eef188f39dc061ef7b0a373280dee8dad041ec2
Pipo Bimbo <heroinedor@gmail.com>
2023-02-03 17:44:22 +0100

    transmission of an id between 2 jobs

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "kubeconfig" {
   value     = ovh_cloud_project_kube.k8s_element_cluster.kubeconfig
   sensitive = true
 }
+
+output "clusterid" {
+  value     = ovh_cloud_project_kube.k8s_element_cluster.id
+}
