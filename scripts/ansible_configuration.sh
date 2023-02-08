ccf62011934e0d7e1cb98be65f83671019573161
Pipo Bimbo <heroinedor@gmail.com>
2023-02-08 12:21:32 +0100

    Configuration with Ansible

---
+++ scripts/ansible_configuration.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this script is intended to run all the configuration stage (after the provisioning stage)
+# - configure kubectl
+# - configure ansible
+# - run ansible playbook
+
+# Configure kubectl
+export WORKING_DIRECTORY=$(pwd)
+echo "WORKING_DIRECTORY=${WORKING_DIRECTORY}"
+export ANSIBLE_ROOT="${WORKING_DIRECTORY}/ansible"
+export KUBECONFIG="${WORKING_DIRECTORY}/kubeconfig.yml"
+echo "KUBECONFIG=${KUBECONFIG}"
+
+cd $ANSIBLE_ROOT
+
+# Configure ansible
+pip3 install kubernetes
+ansible-galaxy collection install -r "requirements.yml"
+
+# Run ansible playbook
+ansible-playbook "playbooks/configuration.yml"
