ac5d4450df27e07348127185ff0ff1736faa3471
Pipo Bimbo <heroinedor@users.noreply.github.com>
2023-03-15 10:43:03 +0100

    Revert "Sprint 1 release (#22)" (#23)
    This reverts commit 3a082699b48dedfe1059dd181f4a564a18b1afdd.

--- scripts/local.env.template.sh
+++ scripts/local.env.template.sh
@@ -20,13 +20,3 @@ export ISSUER_MAIL="<email address used for letsEncrypt/ACME registration>"
 # variables used by matrix components
 export SERVER_SUBDOMAIN_NAME="<the matrix serverName will be : SERVER_SUBDOMAIN_NAME.DNS_ZONE>"
 export ELEMENT_SUBDOMAIN_NAME="<the front-end url will be : ELEMENT_SUBDOMAIN_NAME.DNS_ZONE>"
-export SMTP_HOST="<configuration to send mail : host of the SMTP server>"
-export SMTP_PORT="<configuration to send mail : port of the SMTP server>"
-export SMTP_USER="<configuration to send mail : user to connect to the SMTP server>"
-export SMTP_PASS="<configuration to send mail : password of connect to the SMTP server>"
-# variables used by matrix keycloak
-export KECLOAK_DB_INSTANCE_NAME="<database name of the postgresql database linked to keycloak>"
-export KEYCLOAK_DB_USER="<username to access the postgresql database linked to keycloak>"
-export KEYCLOAK_DB_PASSWORD="<username to access the postgresql database linked to keycloak>"
-export KEYCLOAK_CLIENT_ID="<client ID used by keycloak to identify synapse client>"
-export KEYCLOAK_CLIENT_SECRET="<secret used in conjunction with client ID to identify synapse client>"
