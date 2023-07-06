1637ea00831eccc94afdc39eeaf6c3d77a683c0d
Pipo Bimbo <heroinedor@gmail.com>
2023-07-06 12:41:39 +0200

    #39 : correction on destroy GHA workflow and on script used to retrieve kaycloak passwords

--- scripts/retrieve_keycloak_password.sh
+++ scripts/retrieve_keycloak_password.sh
@@ -2,7 +2,5 @@
 
 # this script is intended to retrieve the password linked to the admin account
 # automatically created during the keycloak installation by the keycloak operator
-echo "keycloak admin password = "
-kubectl get secrets -n keycloak keycloak-psc-initial-admin -oyaml | grep password | sed -e "s/  password: //g" | base64 --decode
-echo "\n keycloak database password = "
-terraform output --raw keycloak_user_password
+echo "keycloak admin password = $(kubectl get secrets -n keycloak keycloak-psc-initial-admin -oyaml | grep password | sed -e "s/  password: //g" | base64 --decode)"
+echo "keycloak database password = $(terraform output --raw keycloak_user_password)"
