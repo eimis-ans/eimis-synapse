b5dafce83c56c7440d728aac402b15d28623d086
Pipo Bimbo <heroinedor@gmail.com>
2023-11-22 18:55:06 +0100

    fix: new param added to be compatible with TF1.6

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
   required_version = ">= 1.4.6"
 }
